636335fd117bed724c4fd2054198f866c40ec2db
naveendev9944 <naveendev994474@gmail.com>
2023-10-18 19:23:54 +0530

    Almost

--- Hospital.java
+++ Hospital.java
@@ -1,24 +1,49 @@
 import java.util.*;
+import java.io.*;
 
-class Illness{
-	private String name;
-	private boolean fatal;
-	private Illness(String name,boolean fatal){
-		this.name=name;
-		this.fatal=fatal;
-	}
-	boolean isFatal(){
-		return this.fatal;
-	}	
 
-	String getName(){
-		return this.name;
-	}
-}
 class IllnessFactory{
-        public static Illness getIllness(String illnessName) throws UnknownIllnessException {
-
+	static List<Illness> illobj=new ArrayList<>();
+        public static Illness getIllness(byte illness){
+		return illobj.get(illness-1);
+		
         }
+	void printIllness(){
+		int i=1;
+		for(Illness ill : illobj){
+			System.out.println(i+"."+ill.name);
+			i++;
+		}
+	}
+	public  void loadIllnessesFromFile() throws IOException {
+        	BufferedReader reader = new BufferedReader(new FileReader("sample.txt"));
+        	String line;
+        	while ((line = reader.readLine()) != null) {
+            	String[] parts = line.split(":");
+            		if (parts.length == 2) {
+                		String name = parts[0];
+               			boolean isFatal = Boolean.parseBoolean(parts[1]);
+                		Illness illness = new Illness(name, isFatal);
+				illobj.add(illness);
+           		}
+        	}
+        	reader.close();
+    	}
+    	class Illness{
+		String name;
+		boolean fatal;
+		private Illness(String name,boolean fatal){
+			this.name=name;
+			this.fatal=fatal;
+		}
+		boolean isFatal(){
+			return this.fatal;
+		}	
+
+		String getName(){
+			return this.name;
+		}
+	}
 }
 
 enum Gender{
@@ -27,29 +52,97 @@ enum Gender{
 }	
 class Patient{
 	String name;
-	Gender genter;
-	Set<Illness> illness=new HashSet<>();
-	boolean servived;
-
+	Gender gender;
+	Set<IllnessFactory.Illness> illness;
+	boolean survived;
 	Patient(String name,byte g,boolean survived){
 		this.name=name;
 		this.gender=Gender.values()[g-1];
+		this.illness=new HashSet<>();
 		this.survived=survived;
 	}
-	void addIllness(Illness ill){
+	Set<IllnessFactory.Illness> getSet(){
+		return this.illness;
+	}
+	String getName(){
+                return this.name;
+        }
+
+	void addIllness(IllnessFactory.Illness ill){
 		illness.add(ill);
 	}
 
-}
-class Hospital{
-	private List
-	public List<Patient> getPatientsBy(Illness illness){
-		
+	void removeIllness(IllnessFactory.Illness ill){
+                illness.remove(ill);
+        }
+
+/*	Illness hasIllness(String illness){
+		return illness.getName().equals(illness);
+	}*/
+
+	boolean hasSurvived(){
+                        return this.survived;
+        }
+	void printp(List<Patient> p1){
+		for(Patient p:p1){
+			System.out.println(p.name);
+		}
 	}
+
+	boolean hasFatalIllness(){
+                for (IllnessFactory.Illness ill : illness)
+			if(ill.isFatal())
+                        	return true;
+        	return false;
+	}
+
+}
+public class Hospital{
+	private List<Patient> patients=new ArrayList<>();
+	Patient getPatient(String name){
+		for(Patient p:patients){
+			if(p.getName().equals(name))
+				return p;
+		}
+	return null;
+	}
+
+	public void addPatient(Patient object){
+		patients.add(object);
+	}
+
+	public List<Patient> getPatientsBy(IllnessFactory.Illness illness){
+	       	List<Patient> patientswithillness = new ArrayList<>();
+        	for (Patient patient : patients) {
+            		if (patient.getSet().contains(illness)){
+                		patientswithillness.add(patient);
+            		}
+        	}
+        	return patientswithillness;
+	}
+       
 	public List<Patient> getAllFatalIllnessSurvivors(){
-	
-	}
-	public List<Patient> getAllFatalIllnessSurvivorsByGender(Gender g) {
-		
-	}
+        	List<Patient> survivors = new ArrayList<>();
+        	for (Patient patient : patients){
+            		if(patient.hasSurvived() && patient.hasFatalIllness()){
+                		survivors.add(patient);
+            		}
+        	}
+        	return survivors;
+    	}
+
+    	public List<Patient> getAllFatalIllnessSurvivorsByGender(Gender gender){
+        	List<Patient> survivorsbygender = new ArrayList<>();
+        	for (Patient patient : patients){
+            		if(patient.gender == gender && patient.hasSurvived() && patient.hasFatalIllness()){
+                		survivorsbygender.add(patient);
+            		}
+        	}
+        	return survivorsbygender;
+    	}	
+    	void showp(List<Patient> p){
+    		patients.get(0).printp( p);
+    	}
+   
 }
+
--- Main.java
+++ Main.java
@@ -1,43 +1,79 @@
 import java.util.*;
-public class Testmain {
-    public static void main(String[] args) {
-	Scanner sc = new Scanner (System.in);
-	Hospital obj=new Hospital();
-   	boolean loop = true;
-    	while (loop)
-      	   {
-		System.out.println("1. Add Patient \n2.Add illness \n3.Print patients \n4.Show All Fatal Illness Survivors\n5.Show AllFatal Illness Survivors By Gender\n6. Exit");
-		int choice = sc.nextInt();
-		sc.nextLine();
-		switch (choice){
-	  		case 1:{
-	     		        System.out.println ("Enter the patient Name:");
-	     		        String name=sc.nextLine();
-	     		        System.out.println ("Enter the gender:");
-	     		        String gender=sc.nextLine();
-	     			break;
-	   		 }
-	 		 case 2:{
-	 		 	
-	     			break;
-	    		}
+import java.io.*;
+class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner (System.in);
+        Hospital obj=new Hospital();
+	IllnessFactory  factory=new IllnessFactory();
+        boolean loop = true;
+        factory.loadIllnessesFromFile();
+        while (loop)
+           {
+                System.out.println("1. Add Patient \n2.Add illness \n3.Print patients by illness \n4.Show All Fatal Illness Survivors\n5.Show AllFatal Illness Survivors By Gender\n6. Exit");
+                int choice = sc.nextInt();
+                sc.nextLine();
+                switch (choice){
+                        case 1:{
+                                System.out.println ("Enter the patient Name:");
+                                String name=sc.nextLine();
+                                System.out.println ("Enter the gender:\n1.Male\n2.Female");
+                                byte gender=sc.nextByte();
+				System.out.println("Survived by the illness\t1.Yes\t 0.No");
+				byte survive=sc.nextByte();
+				System.out.println("Enter the illness to be add");
+				factory.printIllness();
+				byte ill=sc.nextByte();
+                                sc.nextLine();
+				Patient pat=new Patient(name,gender,survive==1?true:false);
+				pat.addIllness(factory.getIllness(ill));
+				obj.addPatient(pat);
+                                break;
+                         }
+                         case 2:{
+                                System.out.println ("Enter the patient Name:");
+                                String name=sc.nextLine();
+                                Patient p=obj.getPatient(name);
+                                if(p==null){
+                                        System.out.println("Patient not found");
+                                        break;
+                                }
+                                System.out.println("Enter the illness to be add");
+                                factory.printIllness();
+                                byte ill=sc.nextByte();
+                                sc.nextLine();
+                                p.addIllness(factory.getIllness(ill));
+                                break;
+                        }
 
-	  		case 3:{
-	    
-	      			break;
-	    		}
-	    		case 4:{
-	      		
-	      			break;
-	    		}
-	    		case 5:{
-	    
-	      			break;
-	    		}
-	  		case 6:{
-	      			loop=false;
-	 		 }
-      	   }
+                        case 3:{
+				System.out.println("Enter the illness");
+                                factory.printIllness();
+                                byte ill=sc.nextByte();
+                                sc.nextLine();
+                                List<Patient> patients=obj.getPatientsBy((factory.getIllness(ill)));
+                                System.out.println("\nPatients by illness\n");
+                                obj.showp(patients);
+                                break;
+                        }
+                        case 4:{
+                                List<Patient> patients=obj.getAllFatalIllnessSurvivors();
+                                System.out.println("\nAll Fatal Illness Survivors\n");
+                                obj.showp(patients);
+                                break;
+                        }
+                        case 5:{
+				System.out.println ("Enter the gender:\n1.Male\n2.Female");
+                                byte g=sc.nextByte();
+                                List<Patient> patients=obj.getAllFatalIllnessSurvivorsByGender(Gender.values()[g-1]);
+                                System.out.println("\nAll Fatal Illness Survivors  By Gender\n");
+                                obj.showp(patients);
+                                break;
+                        }
+                        case 6:{
+                                loop=false;
+                        }
+           }
        }
     }
 }
+
